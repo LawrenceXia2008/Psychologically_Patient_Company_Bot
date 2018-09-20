6fcf36af82d6087016203b0a5fb039179be63b08
LawrenceXia2008 <31108411+LawrenceXia2008@users.noreply.github.com>
2018-09-20 11:34:35 +0800

    Add files via upload

---
+++ Unity3D HCI/AnimationController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using System;
+using System.Threading;
+
+
+namespace game
+{
+    public class AnimationController : MonoBehaviour
+    {
+
+        private Animator m_Animator = null;
+        Myprocess my = new Myprocess();
+        private int ll = 0;
+        private int tag = 0;
+
+        IEnumerator AnimationWait()
+        {
+            yield return new WaitForSeconds(6.0f);
+            m_Animator.SetInteger("animation", 31);
+        }
+
+        void Start()
+        {
+            m_Animator = GetComponent<Animator>();//获取当前对象的Animator  
+        }
+
+
+        void Update()
+        {
+
+            my.start1();
+            if (ll != my.line_num)
+            {
+                if (my.ctrl == 0 && tag == 0)
+                {
+
+                    m_Animator.SetInteger("animation", 15);
+
+                }
+                //if (my.ctrl == 1)
+                //m_Animator.SetInteger("animation", 2);
+                //if (my.ctrl == 2)
+                //m_Animator.SetInteger("animation", 3);
+                if (my.ctrl == 1 && tag == 0)
+                {
+
+                    m_Animator.SetInteger("animation", 25);
+
+                }
+                if (my.ctrl == 2 && tag == 0)
+                {
+
+                    m_Animator.SetInteger("animation", 17);
+
+                }
+                if (my.ctrl == 4 && tag == 0)
+                {
+                    tag = 1;
+                    m_Animator.SetInteger("animation", 1);
+                }
+
+                if (my.ctrl == 5 && tag == 1)
+                {
+                    tag = 0;
+                    StartCoroutine(AnimationWait());
+                }
+
+               
+                ll = my.line_num;
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
---
+++ Unity3D HCI/MyProcess.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace game
+{
+    public class Myprocess : MonoBehaviour
+    {
+        public int ctrl = 0;
+        public int line_num = 0;
+        public void start1()
+        {
+
+            FileStream aFile = new FileStream("C:\\Users\\Paul Xia\\Desktop\\ReadByUnity.txt", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            StreamReader sr = new StreamReader(aFile);
+            String line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                ctrl = int.Parse(line);
+                line_num++;
+                //Thread.Sleep(5000);
+            }
+
+
+
+
+        }
+    }
+}
